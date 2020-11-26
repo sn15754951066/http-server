5385f789df58c69733a6d4b757b434ec82ef7879
zq13408401721 <zq13408401721@139.com>
2020-11-26 10:27:22 +0800

    富文本文字内容+图片+点击事件

--- app/src/main/java/com/umeng/soexample/MainActivity.java
+++ app/src/main/java/com/umeng/soexample/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import com.umeng.soexample.api.ServiceApi;
 import com.umeng.soexample.module.data.CityData;
 import com.umeng.soexample.net.HttpManager;
+import com.umeng.soexample.test.HandlerActivity;
 import com.umeng.soexample.ui.home.HomeActivity;
 import com.umeng.soexample.ui.tongpao.TongpaoActivity;
 import com.umeng.soexample.um.SharedActivity;
@@ -21,7 +22,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    Button btnShared,btnTongpao;
+    Button btnShared,btnTongpao,btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +30,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btnShared = findViewById(R.id.btn_share);
         btnTongpao = findViewById(R.id.btn_tongpao);
+        btnTest = findViewById(R.id.btn_test);
         btnShared.setOnClickListener(this);
         btnTongpao.setOnClickListener(this);
+        btnTest.setOnClickListener(this);
        /* Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);*/
     }
@@ -45,6 +48,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_tongpao:
                 Intent intent1 = new Intent(MainActivity.this, TongpaoActivity.class);
                 startActivity(intent1);
+                break;
+            case R.id.btn_test:
+                Intent intent2 = new Intent(MainActivity.this, HandlerActivity.class);
+                startActivity(intent2);
                 break;
         }
     }
--- app/src/main/java/com/umeng/soexample/test/HandlerActivity.java
+++ app/src/main/java/com/umeng/soexample/test/HandlerActivity.java
@@ -3,9 +3,25 @@ package com.umeng.soexample.test;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.ImageSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.umeng.soexample.R;
 
@@ -14,15 +30,22 @@ import java.util.Map;
 
 public class HandlerActivity extends AppCompatActivity {
     private MyHandler handler = new MyHandler();
+
+    TextView txtMsg,txtLine,txtImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handler);
+        txtMsg = findViewById(R.id.txt_msg);
+        txtLine = findViewById(R.id.txt_line);
+        txtImg = findViewById(R.id.txt_img);
 
         initView();
 
         HashMap<String,Integer> map = new HashMap<>();
         map.put("1",1);
+
+        testSpannable();
 
     }
 
@@ -46,6 +69,57 @@ public class HandlerActivity extends AppCompatActivity {
         public void handleMessage(@NonNull Message msg) {
             super.handleMessage(msg);
         }
+    }
+
+
+
+    private void testSpannable(){
+        String word = "hello adroid";
+        //1 spannable
+        SpannableString spannableString = new SpannableString(word);
+        spannableString.setSpan(new BackgroundColorSpan(Color.RED),0,5, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        txtMsg.setText(spannableString);
+
+
+        //登录注册的阅读链接
+        String txt = "我已阅读《网络安全手册》";  //检索
+        //起始位置
+        int startPos = txt.indexOf("《");
+        //结束位置
+        int endPos = txt.lastIndexOf("》")+1;
+
+        SpannableStringBuilder stringBuilder = new SpannableStringBuilder(txt);
+        stringBuilder.setSpan(new ForegroundColorSpan(Color.BLUE),startPos,endPos,Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+
+        //点击富文本
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                //响应点击事件
+                Log.i("TAG","onClick");
+            }
+        };
+
+        stringBuilder.setSpan(clickableSpan,startPos,endPos,Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        txtLine.setMovementMethod(LinkMovementMethod.getInstance());
+        txtLine.setText(stringBuilder);
+
+        String msg = "你好！[机器人](机器人)";
+        //添加富文本的图片
+        SpannableString imgSpannStr = new SpannableString(msg);
+        //drawable <-> bitmap <-> resouce  图片格式的转换
+        /*Bitmap bmp = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+        bmp.setWidth(20);
+        bmp.setHeight(20);*/
+        Drawable drawable = getResources().getDrawable(R.mipmap.ic_launcher);
+        drawable.setBounds(0,0,20,20);
+        //imageSpan
+        ImageSpan imageSpan = new ImageSpan(drawable);
+        int start = msg.indexOf("[");
+        int end = msg.lastIndexOf("]")+1;
+        imgSpannStr.setSpan(imageSpan,start,end,Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        txtImg.setText(imgSpannStr);
+
     }
 
 
