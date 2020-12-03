a1135820954f8d4c839a8944c3b32d8508644b11
zq13408401721 <zq13408401721@139.com>
2020-12-03 16:59:15 +0800

    路径起点终点搜索+路径规划

--- app/src/main/java/com/umeng/soexample/ui/map/MapActivity.java
+++ app/src/main/java/com/umeng/soexample/ui/map/MapActivity.java
@@ -5,7 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -50,6 +52,10 @@ import com.baidu.mapapi.search.route.TransitRouteResult;
 import com.baidu.mapapi.search.route.WalkingRouteLine;
 import com.baidu.mapapi.search.route.WalkingRoutePlanOption;
 import com.baidu.mapapi.search.route.WalkingRouteResult;
+import com.baidu.mapapi.search.sug.OnGetSuggestionResultListener;
+import com.baidu.mapapi.search.sug.SuggestionResult;
+import com.baidu.mapapi.search.sug.SuggestionSearch;
+import com.baidu.mapapi.search.sug.SuggestionSearchOption;
 import com.umeng.soexample.R;
 import com.umeng.soexample.base.BaseAdapter;
 
@@ -74,12 +80,15 @@ public class MapActivity extends AppCompatActivity implements View.OnClickListen
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
 
+    //路径规划
     @BindView(R.id.input_start)
     EditText inputStart;
     @BindView(R.id.input_end)
     EditText inputEnd;
     @BindView(R.id.btn_routePlan)
     Button btnRoutePlan;
+    @BindView(R.id.recy_nodes)
+    RecyclerView recyNodes;
 
     //百度地图的数据操作
     BaiduMap baiduMap;
@@ -259,14 +268,123 @@ public class MapActivity extends AppCompatActivity implements View.OnClickListen
 
     /*******************************路径规划*************************/
     private PlanNode startNode,endNode; //开始和结束的坐标点
+    SuggestionSearch suggestionSearch; //地点检索的类
+    SuggestAdapter suggestAdapter; //路径规划搜索出来的列表
+    List<SuggestionResult.SuggestionInfo> suggestList; //地点检索的结果
+    boolean isStart = true; //当前处理的是否是起点
+    LatLng startLatLng; //起点的经纬度
 
 
     //初始化路径规划
     private void initRoutePlan(){
+
+        suggestionSearch = SuggestionSearch.newInstance();
+        suggestList = new ArrayList<>();
+        suggestAdapter = new SuggestAdapter(this,suggestList);
+        recyNodes.setLayoutManager(new LinearLayoutManager(this));
+        recyNodes.setAdapter(suggestAdapter);
+        //设置监听地点检索
+        suggestionSearch.setOnGetSuggestionResultListener(suggestionResultListener);
+
+        inputStart.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    isStart = true;
+                    recyNodes.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        //监听起点输入框的变化
+        inputStart.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //起点输入改变以后获取对应的起点数据
+                SuggestionSearchOption option = new SuggestionSearchOption();
+                option.city("北京");
+                option.citylimit(true);
+                option.keyword(s.toString());
+                suggestionSearch.requestSuggestion(option);
+            }
+        });
+        //监听终点输入框的光标
+        inputEnd.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    isStart = false;
+                    recyNodes.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        //监听终点输入框的输入
+        inputEnd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //获取终点框对应的数据
+                SuggestionSearchOption option = new SuggestionSearchOption();
+                option.city("北京");
+                option.citylimit(true);
+                option.keyword(s.toString());
+                suggestionSearch.requestSuggestion(option);
+            }
+        });
+
+
         routePlanSearch = RoutePlanSearch.newInstance();
         routePlanSearch.setOnGetRoutePlanResultListener(routePlanResultListener);
+
+        suggestAdapter.addListClick(new BaseAdapter.IListClick() {
+            @Override
+            public void itemClick(int pos) {
+                SuggestionResult.SuggestionInfo suggestionInfo = suggestList.get(pos);
+                if(isStart){
+                    inputStart.setText(suggestionInfo.getKey());
+                    startLatLng = suggestionInfo.getPt();
+                }else{
+                    inputEnd.setText(suggestionInfo.getKey());
+                }
+                recyNodes.setVisibility(View.GONE);
+            }
+        });
     }
 
+    /**
+     * 地点检索监听
+     */
+    OnGetSuggestionResultListener suggestionResultListener = new OnGetSuggestionResultListener() {
+        @Override
+        public void onGetSuggestionResult(SuggestionResult suggestionResult) {
+            //地点检索结果
+            suggestList.clear();
+            suggestList.addAll(suggestionResult.getAllSuggestions());
+            suggestAdapter.notifyDataSetChanged();
+        }
+    };
+
+    /**
+     * 路径搜索的监听
+     */
     OnGetRoutePlanResultListener routePlanResultListener = new OnGetRoutePlanResultListener() {
         @Override
         public void onGetWalkingRouteResult(WalkingRouteResult walkingRouteResult) {
@@ -276,8 +394,14 @@ public class MapActivity extends AppCompatActivity implements View.OnClickListen
             WalkingRouteOverlay walkingRouteOverlay = new WalkingRouteOverlay(baiduMap);
             //判断当前查找出来的路线
             if(walkingRouteResult.getRouteLines() != null && walkingRouteResult.getRouteLines().size() > 0){
-                walkingRouteOverlay.setData(walkingRouteResult.getRouteLines().get(0));
+                WalkingRouteLine walkingRouteLine = walkingRouteResult.getRouteLines().get(0);
+                walkingRouteOverlay.setData(walkingRouteLine);
                 walkingRouteOverlay.addToMap();
+                //当前的定位移动到开始点并放大地图
+                MapStatusUpdate status = MapStatusUpdateFactory.newLatLngZoom(startLatLng,16);
+                baiduMap.setMapStatus(status);
+            }else{
+                Toast.makeText(MapActivity.this,"未搜索到相关路径",Toast.LENGTH_SHORT).show();
             }
         }
 
@@ -360,24 +484,6 @@ public class MapActivity extends AppCompatActivity implements View.OnClickListen
         //在地图上添加Marker，并显示
         baiduMap.addOverlay(option);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
---
+++ app/src/main/java/com/umeng/soexample/ui/map/SuggestAdapter.java
@@ -0,0 +1,30 @@
+package com.umeng.soexample.ui.map;
+
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.baidu.mapapi.search.sug.SuggestionResult;
+import com.umeng.soexample.R;
+import com.umeng.soexample.base.BaseAdapter;
+
+import java.util.List;
+
+public class SuggestAdapter extends BaseAdapter<SuggestionResult.SuggestionInfo> {
+
+    public SuggestAdapter(Context context, List<SuggestionResult.SuggestionInfo> list){
+        super(context,list);
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.layout_map_suggest_item;
+    }
+
+    @Override
+    protected void bindData(SuggestionResult.SuggestionInfo data, VH vh) {
+        ImageView imgType = (ImageView) vh.getViewById(R.id.img_type);
+        TextView txtName = (TextView) vh.getViewById(R.id.txt_name);
+        txtName.setText(data.getKey());
+    }
+}
